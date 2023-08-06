6055ac1bd5a8516eb862c8195d28d83c66fb9c4d
AngelLam1130 <123198542+AngelLam1130@users.noreply.github.com>
2023-08-06 00:04:00 -0700

    angel_upload&download button function edit

--- frontend/src/components/UploadedMusic.js
+++ frontend/src/components/UploadedMusic.js
@@ -6,28 +6,45 @@ const UploadedMusic = ({}) => {
     const[file,setFile] = useState()
     const {  } = useContext(Context)
 
-    function handleDownload(e) {
-        //need to be changed
+    function handleDownload(version) {
+        // Replace 'url' with the actual endpoint to download the processed file
+        const downloadURL = `url/version/${version}`;
+
+        fetch(downloadURL)
+        .then((response) => response.blob()) // Get the response as a blob
+        .then((blob) => {
+            // Create a download link and trigger the download
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', `Music_Version_${version}.mp3`);
+            document.body.appendChild(link);
+            link.click();
+        })
+        .catch((error) => {
+            console.error('Error downloading file:', error);
+        });
     }
 
     function handleUploadMusicFile() {
-        const formData = new FormData()
-        formData.append('file',file)
-        fetch(
-            'url',
-            {
-                method:"POST",
-                body:formData
-            }
-        ).then((response) => response.json())
-        .then(
-            (result)=>{
-                console.log('success',result)
-            }
-        )
-        .catch(error => {
-            console.error("Error:",error)
+        const formData = new FormData();
+        formData.append('file', file);
+
+        // Replace 'uploadUrl' with the actual endpoint to handle file upload
+        const uploadUrl = 'uploadUrl';
+
+        fetch(uploadUrl, {
+            method: 'POST',
+            body: formData,
         })
+            .then((response) => response.json())
+            .then((result) => {
+                console.log('Upload success:', result);
+            // Perform any other actions after successful upload, if needed
+            })
+            .catch((error) => {
+                console.error('Error uploading file:', error);
+            });
     }
 
     function handleMusicFile(event) {
@@ -45,26 +62,20 @@ const UploadedMusic = ({}) => {
             <form onSubmit={handleUploadMusicFile}>
                 <div className="form_container">
                     <input type="file" name="file" className="form_element" onChange={handleMusicFile} />
-                    <button className="form_element">Generate from Uploaded Music</button>
+                    <button className="form_element" type="submit">Generate from Uploaded Music</button>
                 </div>
             </form>
             <div className="optionChoice">
                 <h3>Music Version 1</h3>
-                <button className="form_element" onClick={handleDownload}>
-                    <span>Download</span>
-                </button>
+                <button className="form_element" onClick={() => handleDownload(1)}>Download</button>
             </div>
             <div className="optionChoice">
                 <h3>Music Version 2</h3>
-                <button className="form_element" onClick={handleDownload}>
-                    <span>Download</span>
-                </button>
+                <button className="form_element" onClick={() => handleDownload(2)}>Download</button>
             </div>
             <div className="optionChoice">
                 <h3>Music Version 3</h3>
-                <button className="form_element" onClick={handleDownload}>
-                    <span>Download</span>
-                </button>
+                <button className="form_element" onClick={() => handleDownload(3)}>Download</button>
             </div>
         </nav>
     )
