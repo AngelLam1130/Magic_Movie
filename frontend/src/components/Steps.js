d19f36e2530aa8ae2ce33cc28a0a3b9abad82de1
Kajari Ghosh <ghoshkaj@gmail.com>
2023-08-06 11:46:13 -0700

    get rid of some errors

--- frontend/src/components/Steps.js
+++ frontend/src/components/Steps.js
@@ -4,7 +4,7 @@ import './Steps.css'
 const Steps = ({ count = 0 }) => {
     console.log("count");
     console.log(count);
-    let content = [...Array(count)].map((el, i) => <div className="step" key={i + 1}>{i + 1}</div>)
+    // let content = [...Array(count)].map((el, i) => <div className="step" key={i + 1}>{i + 1}</div>)
 
     return (
         <div className="steps">
