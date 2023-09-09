25e3b1a1b96cbb5220912d007b9ad275ed4cae66
AngelLam1130 <123198542+AngelLam1130@users.noreply.github.com>
2023-09-09 12:53:53 -0700

    Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,93 +1,21 @@
-import React, { useState, useEffect } from 'react'
-// import ToolBar from './components/Toolbar'
-import UploadedMusic from './components/UploadedMusic'
-import Steps from './components/Steps'
-// import TrackList from './components/TrackList'
-// import PlayHead from './components/PlayHead'
-import { Provider } from './hooks/useStore'
-import useTimer from './hooks/useTimer'
-import useStyles from './hooks/useStyles'
-import './App.css'
+import { SeiWalletProvider } from '@sei-js/react';
+import './App.css';
+import Home from './components/Home';
+import React from 'react';
 
 function App() {
-
-    const baseBPMPerOneSecond = 60
-    const stepsPerBar = 8
-    const beatsPerBar = 4
-    const barsPerSequence = 2
-    const totalSteps = stepsPerBar * barsPerSequence
-    const totalBeats = beatsPerBar * barsPerSequence
-
-    const [BPM, setBPM] = useState(120)
-    const [startTime, setStartTime] = useState(null)
-    const [pastLapsedTime, setPastLapse] = useState(0)
-    const [currentStepID, setCurrentStep] = useState(null)
-    const [getNotesAreaWidthInPixels] = useStyles(totalSteps)
-
-    const notesAreaWidthInPixels = getNotesAreaWidthInPixels(totalSteps)
-    const timePerSequence = baseBPMPerOneSecond / BPM * 1000 * totalBeats
-    const timePerStep = timePerSequence / totalSteps
-    const isSequencePlaying = startTime !== null
-    const playerTime = useTimer(isSequencePlaying)
-    const lapsedTime = isSequencePlaying ? Math.max(0, playerTime - startTime) : 0
-    const totalLapsedTime = pastLapsedTime + lapsedTime
-
-    useEffect(() => {
-        if (isSequencePlaying) {
-            setCurrentStep(Math.floor(totalLapsedTime / timePerStep) % totalSteps)
-        } else {
-            setCurrentStep(null)
-        }
-    }, [isSequencePlaying, timePerStep, totalLapsedTime, totalSteps])
-
-    const toolBarProps = {
-        setStartTime,
-        setPastLapse,
-        setBPM,
-        isSequencePlaying,
-        startTime,
-        BPM
-    }
-
-    const playHeadProps = {
-        notesAreaWidthInPixels,
-        timePerSequence,
-        totalLapsedTime
-    }
-
-    const trackListProps = {
-        currentStepID
-    }
-
-    const uploadedMusicProps = {
-        // setStartTime,
-        // setPastLapse,
-        // setBPM,
-        // isSequencePlaying,
-        // startTime,
-        // BPM
-        //need to be changed
-    }
-
     return (
-        <Provider>
-            <main className="app">
-                <header className="app_header">
-                    <h1 className="app_title">R.A.C.K.</h1>
-                    <h3 className="app_text">Neural Music Inverter 🧠</h3>
-                    {/* <ToolBar {...toolBarProps} /> */}
-                </header>
-                <Steps count={totalSteps} />
-                <div className="app_content">
-                    {/* <PlayHead {...playHeadProps} />
-                    <TrackList {...trackListProps} /> */}
-                    <UploadedMusic {...uploadedMusicProps} />
-                </div>
-                <footer className="app_footer">
-                </footer>
-            </main >
-        </Provider>
-    )
+    	// Set up SeiWalletProvider for easy wallet connection and to use hooks in @sei-js/react
+        <SeiWalletProvider
+            chainConfiguration={{
+                chainId: 'atlantic-2',
+                restUrl: 'https://rest.atlantic-2.seinetwork.io',
+                rpcUrl: 'https://rpc.atlantic-2.seinetwork.io'
+            }}
+            wallets={['compass', 'fin']}>
+	            <Home />
+	    </SeiWalletProvider>
+    );
 }
 
-export default App
+export default App;
